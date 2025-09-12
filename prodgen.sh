0876ad2ba9ba16a6a56c2d97551eff87dcea3650
WindRk511 <tchelminowindrick@gmail.com>
2025-09-12 20:02:48 +0300

    0

--- prodgen.sh
+++ prodgen.sh
@@ -72,20 +72,26 @@ do
 	NSOL=$(echo $p_sol | tr ',' '\n' | wc -l)
 	echo "le $NSOL"
 	i=1
+	t=1
 	while [[ $i -le $NSOL ]]
 	do
 		ref=$(echo $p_sol | cut -d"," -f $i )
-		if [[ $p_type -lt 0 ]] || [[ $p_type -gt 6 ]] || [[ ! "${p_type}" =~ ^[0-9]+$ ]]
+		if [[ $ref -lt 0 ]] || [[ $ref -gt 4 ]] || [[ ! $ref =~ ^[0-9]+$ ]]
 		then
 			echo "\e[31mCHOIX ${i} INCORRECT !!!\e[0m\n"
 			echo "Rechoisir svp"
 			echo "Choix le numero de le type"
 			break
 		fi
+	
+		if [[ $i -ge $NSOL ]]; then
+			echo "le nombre de choix du type du sol est $NSOL"
+			t=0
+			break
+		fi
 		i=$((i+1))
 	done
-	if [[ $i -ge $NSOL ]]; then
-		echo "le nombre de choix du type du sol est $NSOL"
+	if [[ $t -e 0 ]]; then
 		break
 	fi
 done
@@ -95,32 +101,37 @@ echo "___CHOISIR LE SAISON FAVORABLE___"
 while (true)
 do
 	echo -e "\n"
-	echo "1) Sol sableux"
-	echo "2) Sol argileux"
-	echo "3) Sol limoneux"
-	echo "4)Sol humifère (ou organique)"
+	echo "1) froid" 
+	echo "2) chaud" 
+	echo "3) pluie" 
+	echo "4) sec"
 	echo "0) INDEFINIT"
 	echo -n "Entrer votre choix "
 	echo -ne "(NB:Si choix multiple,separer par virgule) : "
-	read p_sol
+	read p_sais
 	
-	NSOL=$(echo $p_sol | tr ',' '\n' | wc -l)
-	echo "le $NSOL"
+	NSAIS=$(echo $p_sais | tr ',' '\n' | wc -l)
 	i=1
-	while [[ $i -le $NSOL ]]
+	while [[ $i -le $SAIS ]]
 	do
-		ref=$(echo $p_sol | cut -d"," -f $i )
-		if [[ $p_type -lt 0 ]] || [[ $p_type -gt 6 ]] || [[ ! "${p_type}" =~ ^[0-9]+$ ]]
+		ref=$(echo $p_sais | cut -d"," -f $i )
+		if [[ $ref -lt 0 ]] || [[ $ref -gt 4 ]] || [[ ! "$ref" =~ ^[0-9]+$ ]]
 		then
 			echo "\e[31mCHOIX ${i} INCORRECT !!!\e[0m\n"
 			echo "Rechoisir svp"
 			echo "Choix le numero de le type"
 			break
 		fi
+	
+		if [[ $i -ge $NSAIS ]]; then
+			echo "le nombre de choix du type du sol est $NSAIS"
+			t=0
+			break
+		fi
+
 		i=$((i+1))
 	done
-	if [[ $i -ge $NSOL ]]; then
-		echo "le nombre de choix du type du sol est $NSOL"
+	if [[ $t -e 0 ]]; then
 		break
 	fi
 done
